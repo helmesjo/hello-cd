acddac63563cecbfb288ed04e0de3d7252e13908
Fred Helmesjö <helmesjo@live.com>
2017-04-26 16:43:31 +0200

    Fixed issue with docker group not having permissions for /var/run/docker.sock

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Add user 'go' to group docker to give permission to call docker-command
-gpasswd -a go docker
+echo "Setting up docker..."
 
-# Start docker daemon
+# Create group docker if not exists
+getent group docker || groupadd docker
+
+# Add user 'go' to group docker to give permission to call docker-command
+usermod -a -G docker go
+
+# Make sure the group docker is associated with docker.sock
+chgrp docker /var/run/docker.sock
+
+# Restart docker daemon
 service docker start
+
+echo "Done setting up docker."
 
 # Run default entrypoint for gocd-agent
 ./docker-entrypoint.sh
