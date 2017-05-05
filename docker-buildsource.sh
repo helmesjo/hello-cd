01fc75289d5a451419d0b00f218d2aae2e1d11b1
Fred Helmesjö <helmesjo@live.com>
2017-05-05 14:54:24 +0200

    Moved build-step of dockerfile into docker-buildsource.sh, basically making it the single entrypoint for any CI-server.

--- docker-buildsource.sh
+++ docker-buildsource.sh
@@ -4,6 +4,17 @@ set -euxo pipefail
 
 # Get the current working directory and add slash to beginning (to handle windows-inconsistencies)
 CURRENT_WDIR=/$(pwd)
-CONTAINER_WDIR=//tmp
+CONTAINER_WDIR=//source
 
-docker run --rm --volume=$CURRENT_WDIR:$CONTAINER_WDIR --workdir=$CONTAINER_WDIR build-image ./build.sh
+# Build environment
+docker build \
+        --tag=build-image \
+        --file=./Dockerfile.build .
+
+# Compile source
+docker run \
+        --rm \
+        --volume=$CURRENT_WDIR:$CONTAINER_WDIR \
+        --workdir=$CONTAINER_WDIR \
+        build-image \
+        ./build.sh
