d40eb8aea8fd6c16d15171ea323088da31c9dbba
Fred Helmesjö <helmesjo@live.com>
2017-05-05 16:35:59 +0200

    Fixed so that an artifact is produced when running docker-buildsource.sh, with the artifact being a snapshot of whole container that built the source. Currently the source is mounted (thus this is not included in the artifact). Change to copy over instead of mounting. The artifact will land in the current dir of the host (that started the build-script).

--- docker-buildsource.sh
+++ docker-buildsource.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-set -euxo pipefail
+#set -euxo pipefail
 
-# Get the current working directory and add slash to beginning (to handle windows-inconsistencies)
+BUILD_IMAGE="build-image"
+CONTAINER_NAME=container_$BUILD_IMAGE
+ARTIFACT_NAME=artifact_$BUILD_IMAGE
 CURRENT_WDIR=/$(pwd)
 CONTAINER_WDIR=//source
 
 # Build environment
-docker build    --tag build-image \
+docker build    --tag $BUILD_IMAGE \
                 --file ./Dockerfile.build .
 
 # Compile source
-docker run  --rm \
-            --volume $CURRENT_WDIR:$CONTAINER_WDIR \
+docker run  --volume $CURRENT_WDIR:$CONTAINER_WDIR \
             --workdir $CONTAINER_WDIR \
-            build-image \
+            --name $CONTAINER_NAME \
+            $BUILD_IMAGE \
             ./build.sh
+
+# Create artifact
+docker commit $CONTAINER_NAME $ARTIFACT_NAME
+docker save --output artifact.tar $ARTIFACT_NAME
+
+# Clean up leftovers
+docker system prune --force
