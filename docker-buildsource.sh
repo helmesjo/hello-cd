546f14e072b270d00b13d95ea66cbc202398a6be
Fred Helmesjö <helmesjo@live.com>
2017-05-12 17:25:21 +0200

    Fixed docker-buildsource.sh & docker-runtets.sh (entrypoints for CI-server). Got a good feeling for how to do things now (getting source in place, saving image to file (artifact), load it to run tests etc. But, need to clean it a bit.

--- docker-buildsource.sh
+++ docker-buildsource.sh
@@ -3,22 +3,25 @@
 # Don't do below. If anything fails, we still need to clean up
 #set -euxo pipefail
 
-BUILD_IMAGE="build-image"
-CONTAINER_NAME=container_$BUILD_IMAGE
-ARTIFACT_NAME=artifact_$BUILD_IMAGE
-CURRENT_WDIR=/$(pwd)
+BUILD_IMAGE_NAME="build-image"
+CONTAINER_NAME=container_$BUILD_IMAGE_NAME
+ARTIFACT_NAME=artifact_$BUILD_IMAGE_NAME
 CONTAINER_WDIR=//source
 
 # Build environment
-docker build    --tag $BUILD_IMAGE \
+docker build    --tag $BUILD_IMAGE_NAME \
                 --file ./Dockerfile.build .
 
+# Create build container
+docker create   --workdir $CONTAINER_WDIR \
+                --name $CONTAINER_NAME \
+                $BUILD_IMAGE_NAME ./build.sh 
+
+# Copy over source to working dir
+docker cp   ./ $CONTAINER_NAME:$CONTAINER_WDIR
+
 # Compile source
-docker run  --volume $CURRENT_WDIR:$CONTAINER_WDIR \
-            --workdir $CONTAINER_WDIR \
-            --name $CONTAINER_NAME \
-            $BUILD_IMAGE \
-            ./build.sh
+docker start -i $CONTAINER_NAME
 
 # Create artifact
 docker commit $CONTAINER_NAME $ARTIFACT_NAME
--- docker-runtests.sh
+++ docker-runtests.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-set -euxo pipefail
+#set -euxo pipefail
 
 # Get the current working directory and add slash to beginning (to handle windows-inconsistencies)
-CURRENT_WDIR=/$(pwd)
-CONTAINER_WDIR=//tmp
+BUILD_IMAGE_NAME="build-image"
+ARTIFACT_IMAGE_NAME=artifact_$BUILD_IMAGE_NAME
+
+docker load --input ./artifact.tar
 
 docker run  --rm \
-            --volume $CURRENT_WDIR:$CONTAINER_WDIR \
-            --workdir $CONTAINER_WDIR \
-            build-image \
+            $ARTIFACT_IMAGE_NAME \
             ./runtests.sh
+
+docker rmi $ARTIFACT_IMAGE_NAME
