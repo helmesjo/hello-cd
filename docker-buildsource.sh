8f5850343dce0452e242ac3ecc4c122fdbe727ed
Fred Helmesjö <helmesjo@live.com>
2017-05-19 16:33:44 +0200

    Refactored docker-buildsource.sh a bit. Saving image to file is not feasible, since it writes down all layers (only want delta-layer with source & built output), so will have to rethink and clean this up. Wanted to keep stuff local, so in case no server exists, things just work anyways. Reading up on docker registry, which can be hosted both locally or remotely. Will probably aim for something like this, and make sure that the whole buildprocess is agnostic to local vs. remote.

--- docker-buildsource.sh
+++ docker-buildsource.sh
@@ -3,19 +3,23 @@
 # Don't do below. If anything fails, we still need to clean up
 #set -euxo pipefail
 
-BUILD_IMAGE_NAME="build-image"
-CONTAINER_NAME=container_$BUILD_IMAGE_NAME
-ARTIFACT_NAME=artifact_$BUILD_IMAGE_NAME
+COMMIT_HASH=$(git rev-parse HEAD)
 CONTAINER_WDIR=//source
 
 # Build environment
+BUILD_IMAGE_NAME="build-image:"$COMMIT_HASH
 docker build    --tag $BUILD_IMAGE_NAME \
                 --file ./Dockerfile.build .
 
 # Create build container
+CONTAINER_NAME=container_build
 docker create   --workdir $CONTAINER_WDIR \
                 --name $CONTAINER_NAME \
                 $BUILD_IMAGE_NAME ./build.sh 
+
+# Remove any previous artifacts
+ARTIFACT_NAME=artifact_$COMMIT_HASH.tar
+rm -f $ARTIFACT_NAME
 
 # Copy over source to working dir
 docker cp   ./ $CONTAINER_NAME:$CONTAINER_WDIR
@@ -24,9 +28,10 @@ docker cp   ./ $CONTAINER_NAME:$CONTAINER_WDIR
 docker start -i $CONTAINER_NAME
 
 # Create artifact
-docker commit $CONTAINER_NAME $ARTIFACT_NAME
-docker save --output artifact.tar $ARTIFACT_NAME
+ARTIFACT_IMAGE=$BUILD_IMAGE_NAME
+docker commit $CONTAINER_NAME $ARTIFACT_IMAGE
+docker save --output $ARTIFACT_NAME $ARTIFACT_IMAGE
 
 # Clean up leftovers
 docker rm $CONTAINER_NAME
-docker rmi $ARTIFACT_NAME
+docker rmi $ARTIFACT_IMAGE
