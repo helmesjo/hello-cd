7daea1af46dd44c1960b7fd03cf21ea8a98d6803
Fred <--global>
2017-05-20 11:45:58 +0200

    Added build.sh script that just does the cmake-magic and runs the tests. Changed travis & appveyor to use this.

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Read here: https://coderwall.com/p/fkfaqq/safer-bash-scripts-with-set-euxo-pipefail
+set -euxo pipefail
+
+mkdir -p build
+cd build
+cmake ..
+cmake --build . --config Release
+ctest -C Release --output-on-failure
+
+sleep 5
