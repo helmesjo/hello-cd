101fece350fcee87ad063e98a50b0f76b557667d
Fred <helmesjo@live.com>
2017-05-26 07:57:45 +0200

    Added logs to make-scripts-executable

--- make-scripts-executable.sh
+++ make-scripts-executable.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -uxo pipefail
+
+function onExit {
+    sleep 3
+}
+trap onExit EXIT
+
 for f in `find . -name '*.sh' -o -regex './s?bin/[^/]+' -o -regex './usr/sbin/[^/]+' -o -regex './usr/lib/[^/]+' `;do
  ( cd `dirname $f` && git update-index --chmod=+x  `basename $f` )
 done
