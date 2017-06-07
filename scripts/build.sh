6e155b63652818b1910bbc5c136ef1ccf9fef10b
Fred <helmesjo@live.com>
2017-06-07 22:14:05 +0200

    Cmake fix: Fixed buildscript to take config parameter (defaults to Release if none supplied)

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,15 @@
 # Read here: https://coderwall.com/p/fkfaqq/safer-bash-scripts-with-set-euxo-pipefail
 set -euxo pipefail
 
+# Read first argument, but default to Release if none supplied. 
+CONFIG="${1:-Release}"
+
+echo Building config: $CONFIG
+
 mkdir -p build
 cd build
-cmake ..
-cmake --build . --config Release
-ctest --build-config Release --verbose --output-on-failure
+cmake -DCMAKE_BUILD_TYPE=$CONFIG ..
+cmake --build . --config $CONFIG
+ctest --build-config $CONFIG --verbose --output-on-failure
 
 sleep 3
