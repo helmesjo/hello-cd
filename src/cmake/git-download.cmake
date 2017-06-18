4116543d466599f87eb167df5f30662d140a431b
Fred <--global>
2017-06-18 14:31:15 +0200

    Changed git-download.cmake to use git subtree instead of just raw git clone, since that will not behave correctly with nested repositories.

--- src/cmake/git-download.cmake
+++ src/cmake/git-download.cmake
@@ -1,5 +1,23 @@
 find_package(git REQUIRED)
 
+function(execute_git)
+    set(options "")
+    set(oneValueArgs
+        OUTPUT_VARIABLE
+    )
+    set(multiValueArgs
+        COMMAND
+    )
+    cmake_parse_arguments(args "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} ${args_COMMAND}
+        OUTPUT_VARIABLE GIT_RESULT
+    )
+
+    set(${args_OUTPUT_VARIABLE} ${GIT_RESULT} PARENT_SCOPE)
+endfunction()
+
 function(download_repo)
     set(options "")
     set(oneValueArgs
@@ -11,27 +29,44 @@ function(download_repo)
     set(multiValueArgs "")
     cmake_parse_arguments(args "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
+    # If no tag is specified, default to master
+    if(NOT args_TAG)
+        set(args_TAG master)
+    endif()
+
+    execute_git(
+        COMMAND rev-parse --show-toplevel
+        OUTPUT_VARIABLE GIT_ROOT
+    )
+    # Need to remove linebreak
+    string(STRIP ${GIT_ROOT} GIT_ROOT)
+
+    # Make clone-dir path relative to git-root
+    string(REPLACE ${GIT_ROOT}/ "" RELATIVE_CLONE_DIR ${args_CLONE_DIR})
+
+    # Determine if there are local changes, then we need to stash save & pop
+    execute_git(
+        COMMAND diff --shortstat
+        OUTPUT_VARIABLE GIT_HAVE_CHANGES
+    )
+
+    if(GIT_HAVE_CHANGES)
+        message(FATAL_ERROR "Git subtree requires a clean directory; please commit changes before running this.")
+    endif()
+
+    message("Cloning branch ${args_TAG} from ${args_URL} into relative directory ${RELATIVE_CLONE_DIR}:")
+    set(MERGE_MESSAGE "Merged branch ${args_TAG} in repository ${args_URL}.")
     if(NOT EXISTS ${args_CLONE_DIR})
-        message("Cloning repository ${args_URL}...")
-        execute_process(
-            COMMAND ${GIT_EXECUTABLE} clone ${args_URL} ${args_CLONE_DIR}
+        message("\tAdding...")
+        execute_git(
+            COMMAND subtree add --prefix ${RELATIVE_CLONE_DIR} ${args_URL} ${args_TAG} --squash --message ${MERGE_MESSAGE}
+            WORKING_DIRECTORY ${GIT_ROOT}
         )
-    endif()
-
-    if(args_DO_PULL)
-        message("Pulling latest changes...")
-        execute_process(
-            COMMAND ${GIT_EXECUTABLE} pull
-            WORKING_DIRECTORY ${args_CLONE_DIR}
-            TIMEOUT 30
-        )
-    endif()
-
-    if(args_TAG)
-        message("Checking out tag ${args_TAG}...")
-        execute_process(
-            COMMAND ${GIT_EXECUTABLE} checkout ${args_TAG}
-            WORKING_DIRECTORY ${args_CLONE_DIR}
+    else()
+        message("\tUpdating...")
+        execute_git(
+            COMMAND subtree pull --prefix ${RELATIVE_CLONE_DIR} ${args_URL} ${args_TAG} --squash --message ${MERGE_MESSAGE}
+            WORKING_DIRECTORY ${GIT_ROOT}
         )
     endif()
 
