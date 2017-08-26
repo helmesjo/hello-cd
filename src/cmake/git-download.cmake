c52da224bf536c085e9baa7daa02fc6692470945
Fred <helmesjo@live.com>
2017-08-26 11:01:15 +0200

    Fixed git-download to handle depth=1 submodules (if submodules specified) by cloning to temp dir in build-tree, merge submodule(s) with same repo then change the URL to this local path, which is then used by git subtree as before. Added 'meta'-file to git-download repos, placed next to the CLONE_DIR, which contains remote HEAD on successfull download and is used to determine if clone is necessary in future runs.

--- src/cmake/git-download.cmake
+++ src/cmake/git-download.cmake
@@ -3,6 +3,7 @@ find_package(Git REQUIRED)
 function(execute_git)
     set(options "")
     set(oneValueArgs
+        WORKING_DIRECTORY
         OUTPUT_VARIABLE
     )
     set(multiValueArgs
@@ -10,12 +11,83 @@ function(execute_git)
     )
     cmake_parse_arguments(args "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
+    if(NOT args_WORKING_DIRECTORY)
+        set(args_WORKING_DIRECTORY "${CMAKE_BINARY_DIR}")
+    endif()
+
     execute_process(
         COMMAND ${GIT_EXECUTABLE} ${args_COMMAND}
         OUTPUT_VARIABLE GIT_RESULT
+        WORKING_DIRECTORY ${args_WORKING_DIRECTORY}
     )
 
     set(${args_OUTPUT_VARIABLE} ${GIT_RESULT} PARENT_SCOPE)
+endfunction()
+
+function(get_repo_head)
+    set(oneValueArgs
+        URL
+        TAG
+        OUTPUT_VARIABLE
+    )
+    cmake_parse_arguments(args "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    # Get remote HEAD for tag (need some parsing)
+    execute_git(
+        COMMAND ls-remote ${args_URL} ${args_TAG}
+        OUTPUT_VARIABLE REMOTE_HEAD
+    )
+    string(REGEX REPLACE "\t" ";" REMOTE_HEAD ${REMOTE_HEAD})
+    list (GET REMOTE_HEAD 0 REMOTE_HEAD)
+
+    set(${args_OUTPUT_VARIABLE} ${REMOTE_HEAD} PARENT_SCOPE)
+endfunction()
+
+function(check_if_up_to_date)
+    set(oneValueArgs
+        URL
+        TAG
+        CLONE_DIR
+        OUTPUT_VARIABLE
+    )
+    cmake_parse_arguments(args "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    set(${args_OUTPUT_VARIABLE} "FALSE" PARENT_SCOPE)
+    
+    get_repo_head(
+        ${ARGV}
+        OUTPUT_VARIABLE REMOTE_HEAD
+    )
+    
+    # If there is a meta file, and the remote HEAD matches local stored in file, we are up-to-date
+    set(META_PATH "${args_CLONE_DIR}_meta")
+    if(EXISTS "${META_PATH}")
+        file(READ "${META_PATH}" CURRENT_HEAD)
+    else()
+        return()
+    endif()
+
+    if(${CURRENT_HEAD} STREQUAL ${REMOTE_HEAD})
+        set(${args_OUTPUT_VARIABLE} "TRUE" PARENT_SCOPE)
+    endif()
+endfunction()
+
+function(store_repo_head)
+    set(oneValueArgs
+        URL
+        TAG
+        CLONE_DIR
+    )
+    cmake_parse_arguments(args "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    
+    get_repo_head(
+        ${ARGV}
+        OUTPUT_VARIABLE REMOTE_HEAD
+    )
+    set(META_PATH "${args_CLONE_DIR}_meta")
+    file(
+        WRITE "${META_PATH}" "${REMOTE_HEAD}"
+    )
 endfunction()
 
 function(download_repo)
@@ -24,6 +96,7 @@ function(download_repo)
         URL
         TAG
         CLONE_DIR
+        SUBMODULES
     )
     set(multiValueArgs "")
     cmake_parse_arguments(args "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
@@ -45,15 +118,76 @@ function(download_repo)
 
     message("Cloning branch ${args_TAG} from ${args_URL} into relative directory ${RELATIVE_CLONE_DIR}...")
     
-    # Determine if there are local changes, then we need to stash save & pop
+    # Determine if there are local changes, in which case download will be skipped
     execute_git(
         COMMAND diff --shortstat
         OUTPUT_VARIABLE GIT_HAVE_CHANGES
     )
-
     if(GIT_HAVE_CHANGES)
         message(WARNING "Local changes detected, skipping download.\n\tGit subtree requires a clean directory; please commit changes before running this.\n\t${GIT_HAVE_CHANGES}")
+        #return()
+    endif()
+
+    # Compare HEAD on remote URL:TAG with local (if any), skip download if up-to-date
+    check_if_up_to_date(
+        ${ARGV}
+        OUTPUT_VARIABLE IS_UP_TO_DATE
+    )
+    if(IS_UP_TO_DATE)
+        message("Already up-to-date with tag '${args_TAG}' on ${args_URL}\n\tSkipping download...")
         return()
+    endif()
+
+    # Submodules can't be pulled with git subtree, so in this case clone 
+    # normally to temp-dir, download specified submodules and set git-url to this instead of remote.
+    # Note: Must
+    if(args_SUBMODULES)
+        set(TMP_DIR "${CMAKE_BINARY_DIR}/tmp")
+        execute_process(
+            COMMAND ${CMAKE_COMMAND} -E remove_directory ${TMP_DIR}
+            WORKING_DIRECTORY "${TMP_DIR}/.."
+        )
+
+        execute_git(
+            COMMAND clone ${args_URL} "${TMP_DIR}"
+        )
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} checkout ${args_TAG}
+            WORKING_DIRECTORY "${TMP_DIR}"
+        )
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} submodule update --init ${args_SUBMODULES}
+            WORKING_DIRECTORY "${TMP_DIR}"
+        )
+
+        execute_git(
+            COMMAND rm .gitmodules
+            WORKING_DIRECTORY "${TMP_DIR}"
+        )
+
+        # Merge submodules into main repo so that subtree merges it all
+        foreach(SUBMODULE ${args_SUBMODULES})
+            execute_git(
+                COMMAND rm --cached ${SUBMODULE}
+                WORKING_DIRECTORY "${TMP_DIR}"
+            )
+            execute_process(
+                COMMAND ${CMAKE_COMMAND} -E remove ${SUBMODULE}/.git
+                WORKING_DIRECTORY "${TMP_DIR}"
+            )
+        endforeach()
+        
+        execute_git(
+            COMMAND add -A
+            WORKING_DIRECTORY "${TMP_DIR}"
+        )
+        execute_git(
+            COMMAND commit -am "git-download: Merged submodules into main repo for use with subtree."
+            WORKING_DIRECTORY "${TMP_DIR}"
+        )
+
+        set(args_URL "${TMP_DIR}/.git")
+        
     endif()
 
     set(MERGE_MESSAGE "Merged branch ${args_TAG} in repository ${args_URL}.")
@@ -70,5 +204,10 @@ function(download_repo)
             WORKING_DIRECTORY ${GIT_ROOT}
         )
     endif()
+
+    # Assuming everything went well, remember HEAD to compare each future runs
+    store_repo_head(
+        ${ARGV}
+    )
 
 endfunction()
