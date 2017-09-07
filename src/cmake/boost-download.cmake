214ba22126b5c10309d03a83c3a9ace099da78ce
Fred <helmesjo@live.com>
2017-09-07 23:39:49 +0200

    Redid the boost-dl script to now instead do shallow clone & pull, build boostdep then determine required modules and remove any unused (and should then merge with subtree)

--- src/cmake/boost-download.cmake
+++ src/cmake/boost-download.cmake
@@ -1,31 +1,6 @@
 find_package(Git REQUIRED)
 
-# Required modules to build boostdep, which is used to determine when all other dependencies are satisfied
-set(REQUIRED_MODULES 
-    build 
-    boostdep 
-    chrono
-    config 
-    filesystem 
-    system 
-    core 
-    type_traits 
-    predef 
-    assert 
-    iterator 
-    mpl 
-    preprocessor 
-    static_assert 
-    detail
-    smart_ptr
-    throw_exception
-    io
-    functional
-    range
-)
-
 if(WIN32)
-    list(APPEND REQUIRED_MODULES winapi)
     set(BOOTSTRAP_SCRIPT "bootstrap.bat")
 else()
     set(BOOTSTRAP_SCRIPT "bootstrap.sh")
@@ -58,7 +33,7 @@ function(execute_git)
     )
 
     if(NOT "${RETURN_CODE}" STREQUAL "0")
-        message(FATAL_ERROR "Failed to run git command.")
+        message(FATAL_ERROR "Failed to run 'git ${args_COMMAND}'")
     endif()
 
     set(${args_OUTPUT_VARIABLE} ${GIT_RESULT} PARENT_SCOPE)
@@ -95,66 +70,71 @@ function(pull_modules)
     set(oneValueArgs
         REPO_DIR
     )
-    set(multiValueArgs
-        SUBMODULES
-    )
     cmake_parse_arguments(args "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-
-    # Determine correct relative path to each module
-    set(git_SUBMODULES "")
-    foreach(boost_MODULE ${args_SUBMODULES})
-        if(EXISTS "${args_REPO_DIR}/libs/${boost_MODULE}")
-            list(APPEND git_SUBMODULES libs/${boost_MODULE})
-        elseif(EXISTS "${args_REPO_DIR}/tools/${boost_MODULE}")
-            list(APPEND git_SUBMODULES tools/${boost_MODULE})
-        else()
-            message(WARNING "Module '${boost_MODULE}' not found in either libs/ or tools/")
-        endif()
-    endforeach()
 
     execute_git(
-        COMMAND submodule update --depth=1 --init ${git_SUBMODULES}
+        COMMAND submodule update --depth=1 --init
         WORKING_DIRECTORY "${args_REPO_DIR}"
     )
+
 endfunction()
 
-function(get_missing_dependencies)
+function(get_required_modules)
     set(oneValueArgs
         REPO_DIR
-        MISSING_DEPENDENCIES
+        OPTIONAL 
+            ALL_MODULES
+            REQUIRED_MODULES
+            UNUSED_MODULES
     )
     set(multiValueArgs
         SUBMODULES
     )
     cmake_parse_arguments(args "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    set(ALL_MISSING_DEPENDENCIES "")
-    # Run through each module and find missing dependencies
+    set(ALL_REQUIRED_MODULES "")
+    # Run through each module and find required dependencies
     foreach(MODULE ${args_SUBMODULES})
-        message("Finding required dependencies for ${MODULE}...")
+        #message("Finding required dependencies for ${MODULE}...")
         execute_process(
             COMMAND ${BOOSTDEP_EXEC_PATH} --subset ${MODULE}
-            OUTPUT_VARIABLE MISSING_DEPENDENCIES
+            OUTPUT_VARIABLE REQUIRED_MODULES
             WORKING_DIRECTORY ${args_REPO_DIR}
         )
 
         # Pattern-match to find module-name (eg. "assert:"), then remove ":"
-        string(REGEX MATCHALL "[a-z_]*:" MISSING_DEPENDENCIES "${MISSING_DEPENDENCIES}")
-        string(REPLACE ":" "" MISSING_DEPENDENCIES "${MISSING_DEPENDENCIES}")
+        string(REGEX MATCHALL "[a-z_]*:" REQUIRED_MODULES "${REQUIRED_MODULES}")
+        string(REPLACE ":" "" REQUIRED_MODULES "${REQUIRED_MODULES}")
 
         # Only add unique and missing modules
-        foreach(DEPENDENCY ${MISSING_DEPENDENCIES})
-            if(NOT DEPENDENCY IN_LIST ALL_MISSING_DEPENDENCIES AND NOT EXISTS "${args_REPO_DIR}/libs/${DEPENDENCY}/.git")
-                list(APPEND ALL_MISSING_DEPENDENCIES "${DEPENDENCY}")
+        foreach(DEPENDENCY ${REQUIRED_MODULES})
+            if(NOT DEPENDENCY IN_LIST ALL_REQUIRED_MODULES)
+                list(APPEND ALL_REQUIRED_MODULES ${DEPENDENCY})
             endif()
         endforeach()
 
-        message("Required dependencies for ${MODULE}: ${MISSING_DEPENDENCIES}")
+        message("Required modules for ${MODULE}: ${REQUIRED_MODULES}\n")
     endforeach()
     
-    #message("All missing dependencies: \n${ALL_MISSING_DEPENDENCIES}")
+    # List all modules
+    execute_process(
+        COMMAND ${BOOSTDEP_EXEC_PATH} --list-modules
+        OUTPUT_VARIABLE ALL_MODULES
+        WORKING_DIRECTORY ${args_REPO_DIR}
+    )
+    # Convert to list
+    string(REPLACE "\n" ";" ALL_MODULES "${ALL_MODULES}")
+    # Determine which modules are unused
+    set(ALL_UNUSED_MODULES "")
+    foreach(MODULE ${ALL_MODULES})
+        if(NOT MODULE IN_LIST ALL_REQUIRED_MODULES)
+            list(APPEND ALL_UNUSED_MODULES ${MODULE})
+        endif()
+    endforeach()
 
-    set(${args_MISSING_DEPENDENCIES} ${ALL_MISSING_DEPENDENCIES} PARENT_SCOPE)
+    set(${args_ALL_MODULES} ${ALL_MODULES} PARENT_SCOPE)
+    set(${args_REQUIRED_MODULES} ${ALL_REQUIRED_MODULES} PARENT_SCOPE)
+    set(${args_UNUSED_MODULES} ${ALL_UNUSED_MODULES} PARENT_SCOPE)
 endfunction()
 
 
@@ -169,23 +149,27 @@ function(download_boost)
     )
     cmake_parse_arguments(args "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    # 1: Clone repo
-    clone_repo(
-        ${ARGV}
-    )
-    # 2: Pull required modules for boostdep
-    pull_modules(
-        REPO_DIR ${args_CLONE_DIR}
-        SUBMODULES ${REQUIRED_MODULES}
-    )
+    if(NOT EXISTS "${args_CLONE_DIR}")
+        # 1: Clone repo
+        clone_repo(
+            ${ARGV}
+        )
+        # 2: Pull all modules
+        pull_modules(
+            REPO_DIR ${args_CLONE_DIR}
+        )
+    endif()
 
-    file( GLOB IS_BUILT "${args_CLONE_DIR}/b2*" )
-    if(NOT IS_BUILT)
+    file( GLOB IS_B2_BUILT "${args_CLONE_DIR}/b2*" )
+    if(NOT IS_B2_BUILT)
         # 3: Run boostrap
         execute_process(
             COMMAND ${BOOTSTRAP_SCRIPT}
             WORKING_DIRECTORY ${args_CLONE_DIR}
         )
+    endif()
+
+    if(NOT EXISTS "${args_CLONE_DIR}/${BOOSTDEP_EXEC_PATH}")
         # 4: Build (and install) boostdep. Will be put in dist/bin/
         execute_process(
             COMMAND ${BUILD_SCRIPT} "tools/boostdep/build//install"
@@ -193,25 +177,22 @@ function(download_boost)
         )
     endif()
 
-    SET(MODULES_TO_PULL ${args_SUBMODULES})
-    while(MODULES_TO_PULL)
+    # 6: Get missing dependencies for all modules
+    get_required_modules(
+        REPO_DIR ${args_CLONE_DIR}
+        SUBMODULES ${args_SUBMODULES}
+        REQUIRED_MODULES MODULES_NEEDED
+        UNUSED_MODULES MODULES_TO_REMOVE
+    )
 
-        # 5: Pull specified modules
-        pull_modules(
-            REPO_DIR ${args_CLONE_DIR}
-            SUBMODULES ${MODULES_TO_PULL}
-        )
-        # 6: Get missing dependencies for all modules
-        get_missing_dependencies(
-            REPO_DIR ${args_CLONE_DIR}
-            SUBMODULES ${MODULES_TO_PULL}
-            MISSING_DEPENDENCIES MODULES_TO_PULL
-        )
-
-        message("Modules still missing: ${MODULES_TO_PULL}")
-
-    # 7: Repeat 6 until no missing dependencies
-    endwhile()
+    foreach(MODULE ${MODULES_TO_REMOVE})
+        if(NOT MODULE IN_LIST MODULES_NEEDED)
+            file(REMOVE_RECURSE "${args_CLONE_DIR}/libs/${MODULE}")
+        endif()
+    endforeach()
+    
+    message("All required modules: ${MODULES_NEEDED}\n")
+    message("All unused modules (removed): ${MODULES_TO_REMOVE}\n")
 
 endfunction()
 
