0bf83f72685a1244914faccf83f5d8836ac61576
Fred <helmesjo@live.com>
2017-09-12 00:45:43 +0200

    Revert "Made glob-expression for finding removeable folders prettier"
    This reverts commit f7db6e5183837db1dec97ae8ab2e8b9df8707ef2.

--- src/cmake/boost-download.cmake
+++ src/cmake/boost-download.cmake
@@ -109,8 +109,12 @@ function(remove_non_source)
     
     file(GLOB NON_SOURCE_DIRS
         ABSOLUTE "${args_BOOST_DIR}/" 
-            "${args_BOOST_DIR}/libs/*/{doc,test,example}/"
-            "${args_BOOST_DIR}/tools/*/{doc,test,example}/"
+            "${args_BOOST_DIR}/libs/*/doc/"
+            "${args_BOOST_DIR}/libs/*/example/"
+            "${args_BOOST_DIR}/libs/*/test/"
+            "${args_BOOST_DIR}/tools/*/doc/"
+            "${args_BOOST_DIR}/tools/*/example/"
+            "${args_BOOST_DIR}/tools/*/test/"
     )
 
     foreach(DIR ${NON_SOURCE_DIRS})
