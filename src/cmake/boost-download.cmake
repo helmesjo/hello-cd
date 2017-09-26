e7d7de2a7c1c1b8c7a7b23ec86e4222eb2023512
Fred <helmesjo@live.com>
2017-09-26 08:08:43 +0200

    CMake: When boost-download removes test/doc/example folders from libs, it only looks in either the tools or the libs subfolder. Had issues with it removing .git/modules/test

--- src/cmake/boost-download.cmake
+++ src/cmake/boost-download.cmake
@@ -109,7 +109,8 @@ function(remove_non_source)
     
     file(GLOB_RECURSE NON_SOURCE_DIRS
         LIST_DIRECTORIES true
-        "${args_BOOST_DIR}/*"
+        "${args_BOOST_DIR}/libs/*"
+        "${args_BOOST_DIR}/tools/*"
     )
     # Filter away all folder-paths not ending in either "doc", "example" or "test"
     list(FILTER NON_SOURCE_DIRS 
