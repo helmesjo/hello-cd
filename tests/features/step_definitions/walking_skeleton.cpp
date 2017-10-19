d86ca5cbb2bc250d2b53a263d7592b58b290e565
Fred <helmesjo@live.com>
2017-10-19 18:19:30 +0200

    Added 'walking skeleton' test (kind of end-to-end test with bare minimum to confirm that all parts builds & runs together)

---
+++ tests/features/step_definitions/walking_skeleton.cpp
@@ -0,0 +1,41 @@
+#include <boost/test/unit_test.hpp>
+#define EXPECT_EQ BOOST_CHECK_EQUAL
+#include <cucumber-cpp/autodetect.hpp>
+
+#include <mylib/calculator.hpp>
+
+using cucumber::ScenarioScope;
+
+struct CalcCtx {
+    calculator calc;
+    double first;
+    double second;
+    double result;
+};
+
+GIVEN("^I have entered (\\d+) into the calculator$") {
+    REGEX_PARAM(double, n);
+    ScenarioScope<CalcCtx> context;
+
+    context->first = n;
+    context->second = n;
+}
+
+WHEN("^I press add") {
+    ScenarioScope<CalcCtx> context;
+
+    context->result = context->calc.sum(context->first, context->second);
+}
+
+WHEN("^I press divide") {
+    ScenarioScope<CalcCtx> context;
+
+    //context->result = context->calc.divide();
+}
+
+THEN("^the result should be (.*) on the screen$") {
+    REGEX_PARAM(double, expected);
+    ScenarioScope<CalcCtx> context;
+
+    EXPECT_EQ(expected, context->result);
+}
