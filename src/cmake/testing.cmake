a85b67af4458e991466c51b59147339e9fd19037
Fred <helmesjo@live.com>
2017-10-24 21:54:58 +0200

    Refactored the catch-specific stuff out of add_test_internal

--- src/cmake/testing.cmake
+++ src/cmake/testing.cmake
@@ -5,8 +5,10 @@ function(add_test_internal)
     set(oneValueArgs
         TEST_TARGET
         TEST_NAME
+        REPORT_FILE
     )
     set(multiValueArgs
+        TEST_ARGS
         SOURCES
         INCLUDE_DIRS
         TAGS
@@ -32,37 +34,47 @@ function(add_test_internal)
             fakeit
     )
 
-    set(REPORT_FILE "${arg_TEST_NAME}.xml")
     add_test(
         NAME ${arg_TEST_NAME} 
-        COMMAND ${arg_TEST_NAME} --reporter junit --out ${REPORT_FILE}
+        COMMAND ${arg_TEST_NAME} ${arg_TEST_ARGS}
     )
     set_tests_properties(${arg_TEST_NAME} PROPERTIES LABELS ${arg_TAGS})
 
     get_target_property(OUT_DIR ${arg_TEST_NAME} BINARY_DIR)
     install(
-        FILES "${OUT_DIR}/${REPORT_FILE}"
+        FILES "${OUT_DIR}/${arg_REPORT_FILE}"
         DESTINATION ./reports
         OPTIONAL
     )
 
     message("- Test '${arg_TEST_NAME}' setup for target '${arg_TEST_TARGET}'.\n
-    \tTarget found at: \"${OUT_DIR}\".\n \
-    \tReport found at: \"${OUT_DIR}/${REPORT_FILE}\"\n
-    \tTags: \"${arg_TAGS}\".\n"
+    \tTarget found at: \"${OUT_DIR}\"\n \
+    \tReport found at: \"${OUT_DIR}/${arg_REPORT_FILE}\n \
+    \tTags: \"${arg_TAGS}\"\n"
     )
 
 endfunction()
 
+function(add_catch_test)
+    set(oneValueArgs
+        TEST_NAME
+    )
+    cmake_parse_arguments(arg "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    
+    add_test_internal( ${ARGV}
+        TEST_ARGS --reporter junit --out "${arg_TEST_NAME}.xml"
+        REPORT_FILE "${arg_TEST_NAME}.xml"
+    )
+endfunction()
 
 function(add_unit_test)
-    add_test_internal(${ARGV} TAGS "unit")
+    add_catch_test(${ARGV} TAGS "unit")
 endfunction()
 
 function(add_integration_test)
-    add_test_internal(${ARGV} TAGS "integration")
+    add_catch_test(${ARGV} TAGS "integration")
 endfunction()
 
 function(add_performance_test)
-    add_test_internal(${ARGV} TAGS "performance")
+    add_catch_test(${ARGV} TAGS "performance")
 endfunction()
