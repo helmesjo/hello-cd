3193bb26f7e17c322d46ebd2d075d475b1b231a1
Fred <helmesjo@live.com>
2017-10-29 09:55:09 +0100

    Added docker/run-script.sh that takes an argument and runs it inside the container. Useful to run say the different available scripts (inside scripts subfolder) inside a docker container. Will change other docker-scripts to use this (or remove all others and only use this in CI-builds etc).

---
+++ docker/run-script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -uxo pipefail
+
+# Clean up leftovers before exit (don't delete build image, it might be shared. I don't like this though!)
+function cleanup {
+    echo "Cleaning up leftovers..."
+    docker rm $CONTAINER_ID
+    docker rmi $ARTIFACT_ID
+    sleep 3
+}
+trap cleanup EXIT
+
+SCRIPT="${1:-NoScriptSpecified.sh}"
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+COMMIT_HASH=$(git rev-parse --short HEAD)
+CONTAINER_WDIR=//source
+
+# Build environment
+BUILD_IMAGE="build:"$COMMIT_HASH
+docker build    --tag $BUILD_IMAGE \
+                --file $CURRENT_DIR/Dockerfile.build .
+
+# Create build container & compile (create+start instead of run because of issues with logs)
+CONTAINER_ID=$( docker create \
+                --workdir $CONTAINER_WDIR \
+                $BUILD_IMAGE sh -c "$SCRIPT" \
+                )
+docker start -i $CONTAINER_ID
