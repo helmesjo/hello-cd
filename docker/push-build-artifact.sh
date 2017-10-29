2601ea8610d27f448b4111271d27b2af2de59470
Fred <helmesjo@live.com>
2017-10-29 21:20:18 +0100

    Fixed docker/push-build-artifact.sh to forward dockerfile & tag arguments to docker/build-image.sh. Also made docker repository & image-tag optional (arg 1 & 2 respectively)

--- docker/push-build-artifact.sh
+++ docker/push-build-artifact.sh
@@ -8,22 +8,27 @@ function cleanup {
     docker rmi $ARTIFACT_ID
 
     (>&2 echo $IMAGE_TAG)
-
     sleep 3
 }
 trap cleanup EXIT
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_ROOT_DIR=$CURRENT_DIR/..
+REPO_NAME=$(basename `git rev-parse --show-toplevel`)
 COMMIT_HASH=$(git rev-parse --short HEAD)
 CONTAINER_WDIR=//source
-REPO_NAME=$(basename `git rev-parse --show-toplevel`)
 
-IMAGE_ID=$($CURRENT_DIR/build-image.sh 2>&1 >/dev/null)
-DOCKER_REPO="localhost:5000"
-IMAGE_TAG="${1:-$DOCKER_REPO/"$REPO_NAME:"$COMMIT_HASH}"
+# Expected args: 
+# 1. Docker repository
+# 2. Image-tag  (optional)
+# 3. Dockerfile (optional)
+DOCKER_REPO="${1:-"localhost:5000"}"
+IMAGE_TAG="${2:-$REPO_NAME:$COMMIT_HASH}"
+DOCKERFILE="${3:-$CURRENT_DIR/Dockerfile.build}"
 
-echo "Pushing image '$IMAGE_ID' to repository '$DOCKER_REPO'..."
+IMAGE_ID=$($CURRENT_DIR/build-image.sh $DOCKERFILE $IMAGE_TAG 2>&1 >/dev/null)
+
+echo "Pushing image '$IMAGE_TAG' to repository '$DOCKER_REPO'..."
 
 # Create a new container and copy over built files
 CONTAINER_ID=$( docker create \
@@ -35,6 +40,6 @@ docker cp $REPO_ROOT_DIR/build $CONTAINER_ID:$CONTAINER_WDIR
 # Create, tag & push image (the end result, AKA artifact)
 ARTIFACT_ID=$( docker commit    --message $COMMIT_HASH \
                                 $CONTAINER_ID \
-                                $IMAGE_TAG \
+                                $DOCKER_REPO/$IMAGE_TAG \
                                 )
-docker push $IMAGE_TAG
+docker push $DOCKER_REPO/$IMAGE_TAG
