c9cd4bbfd5b90ac1829acb6164d6a4490911737f
Fred <helmesjo@live.com>
2017-10-29 18:40:21 +0100

    Added docker/build-image.sh script for a quick way to build an image (defaults to docker/Dockerfile.build). Trying to break things up into smaller parts, keeps things more reusable and sane.

---
+++ docker/build-image.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Clean up leftovers before exit (don't delete build image, it might be shared. I don't like this though!)
+function cleanup {
+    (>&2 echo "$IMAGE_ID")
+    sleep 1
+}
+trap cleanup EXIT
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+COMMIT_HASH=$(git rev-parse --short HEAD)
+REPO_NAME=$(basename `git rev-parse --show-toplevel`)
+
+DOCKERFILE="${1:-$CURRENT_DIR/Dockerfile.build}"
+IMAGE_TAG="${2:-$REPO_NAME:$COMMIT_HASH}"
+
+echo "Building image '$DOCKERFILE'..."
+
+# Build environment
+IMAGE_ID=$( docker build \
+                --quiet \
+                --tag $IMAGE_TAG \
+                --file $DOCKERFILE .//docker
+                )
