66574580f144e53697936c23cbbe238ca02b6eea
Fred <helmesjo@live.com>
2017-11-13 20:56:06 +0100

    gocd: Fixed permissions for docker-agent go user & docker

--- gocd/agent/custom-entrypoint.sh
+++ gocd/agent/custom-entrypoint.sh
@@ -4,10 +4,16 @@ set -euxo pipefail
 
 echo "Setting up docker..."
 
+# Create group docker if not exists
+getent group docker || groupadd docker
+
+# Add user 'go' to group docker to give permission to call docker-command
+usermod -a -G docker go
+
 # Use storage driver "vfs", with lower performance but guarantees to work correctly with dind
 mkdir /etc/docker/ && echo "{\"storage-driver\":\"vfs\"}" > /etc/docker/daemon.json
 
-# Start docker daemon
+# Restart docker daemon
 service docker start
 
 echo "Done setting up docker."
