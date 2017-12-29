e908f374338ee73688c2b6d20e01aafcd8eb0434
Fred <helmesjo@live.com>
2017-12-29 08:54:22 +0100

    Fixed build.sh to use cmake toolchains to setup environment. Toolchain is determined from current environment (OS, ARCH & COMPILER), and are setup t properly match conan dependencies.

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,29 +11,32 @@ trap on_error ERR
 
 # Read arguments
 CONFIG="${1:-Release}"
-ARCH="${2:-x86}"
+ARCH="${2:-x86_64}"
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TOOLCHAIN=$($CURRENT_DIR/determine-toolchain.sh 2>&1 >/dev/null)
+TOOLCHAIN_DIR="$CURRENT_DIR/../cmake/toolchain"
+BUILD_DIR="$CURRENT_DIR/../build"
 
 # Install dependencies
 $CURRENT_DIR/install-dependencies.sh $CONFIG $ARCH
 
-echo "Building config '$CONFIG'..."
-cmake -E make_directory build
+echo "Building for '$CONFIG $ARCH'..."
+cmake -E make_directory $BUILD_DIR
 # Generate
-cmake -E chdir build \
-    cmake .. -DCMAKE_BUILD_TYPE=$CONFIG -DCMAKE_INSTALL_PREFIX=output -DDOWNLOAD_ENABLED=FALSE
+cmake -E chdir $BUILD_DIR \
+    cmake .. -DCMAKE_BUILD_TYPE=$CONFIG -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_DIR/$TOOLCHAIN.cmake" -DCMAKE_INSTALL_PREFIX=output -DDOWNLOAD_ENABLED=FALSE
 
 # Build
-cmake -E chdir build \
+cmake -E chdir $BUILD_DIR \
     cmake --build . --config $CONFIG
 
 # Run unit tests
-cmake -E chdir build \
+cmake -E chdir $BUILD_DIR \
     ctest --build-config $CONFIG --parallel 2 --label-regex "unit" --output-on-failure
 
 # Install
-cmake -E chdir build \
+cmake -E chdir $BUILD_DIR \
     cmake --build . --target install --config $CONFIG
 
 sleep 3
