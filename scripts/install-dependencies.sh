8ee908f7be214bc80350b19700282d8df77cf487
Fred <helmesjo@live.com>
2017-12-29 08:52:22 +0100

    conan: Fixed install-dependencies.sh to use conan-profiles so that correct settings are used for respective environment. Fixed so that a default profile is added (if missing) that autodetects settings from current environment. Dependencies are installed using CONFIG (Release/Debug) & ARCH (x86_64/x86) taken as arguments, so that dependencies match the cmake build.

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 function on_error {
     echo -e "\n-- Dependencies NOT installed.\n"
-    sleep 5
+    sleep 3
     exit 1
 }
 trap on_error ERR
@@ -15,20 +15,27 @@ command -v conan >/dev/null 2>&1 ||
 }
 
 CONFIG="${1:-Release}"
-ARCH="${2:-x86}"
+ARCH="${2:-x86_64}"
 
-echo -e "\n-- Installing dependencies...\n"
+echo -e "\n-- Installing dependencies for '$CONFIG $ARCH'...\n"
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BUILD_DIR=$CURRENT_DIR/../build
+OS="$($CURRENT_DIR/get-os.sh 2>&1 >/dev/null)"
+PROFILE="$CURRENT_DIR/../conan/profile-$OS.txt"
+BUILD_DIR="$CURRENT_DIR/../build"
 
 # Add conan-community as remote. Needed until more packages are available in the official repository.
 # Fails if already added. If so, just swollow error.
-conan remote add conan_community https://api.bintray.com/conan/conan-community/conan >/dev/null || true
+conan remote add conan_community https://api.bintray.com/conan/conan-community/conan 2>&1 >/dev/null || true
 
-cmake -E make_directory "$BUILD_DIR"
-cmake -E chdir "$BUILD_DIR" \
-    conan install .. --build=missing -s arch=$ARCH -s build_type=$CONFIG
+cmake -E make_directory $BUILD_DIR
+
+# Generate default profile. It is inherited inside profiles to autofill settings
+conan profile new default --detect || true
+
+# Install dependencies. Build if pre-built is missing.
+cmake -E chdir $BUILD_DIR \
+    conan install .. --build=missing -s arch=$ARCH -s build_type=$CONFIG --profile=$PROFILE
 
 echo -e "\n-- Dependencies installed.\n"
 sleep 2
