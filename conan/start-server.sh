a41403dfc0a8354187fe418c9c597aa007c935d8
Fred <helmesjo@live.com>
2018-01-18 22:07:39 +0100

    Added script to start local conan-server (running in docker-container) & automatically connects to custom network 'hello-cd', making it reachable by name by other containers joining the same network.

--- conan/start-server.sh
+++ conan/start-server.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
-function onExit {
-    $SHELL
+function on_error {
+    echo "Could not start conan server '$SERVER_NAME'" >&2
+    sleep 5
+    exit 1
 }
-trap onExit EXIT
+trap on_error ERR
 
 SERVER_NAME="conan-server"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DATA_DIR="$DIR/server/_data"
 
 DOCKERFILE="$DIR/server/Dockerfile"
 IMAGE_NAME="conan-server"
 
-echo "Starting conan server..."
+# Make sure network is started (used to enable communication by container-name)
+NETWORK=$($DIR/../docker/start-network.sh 2>&1)
 
-# Build docker image for the gocd-agent
+echo "Starting conan server, connecting it to network '$NETWORK'..."
+
+# Build docker image for the conan-server
 docker build    --tag $IMAGE_NAME \
                 --file $DOCKERFILE .
 
-mkdir -p "$DATA_DIR"
+# Copy server-config into to-be-mounted conan server-folder
+DATA_DIR="$DIR/server/_data"
+CONFIG_FILE="$DIR/server/server.conf"
+CONFIG_CONAN_PATH="$DATA_DIR/.conan_server/"
+mkdir -p $CONFIG_CONAN_PATH && cp $CONFIG_FILE $CONFIG_CONAN_PATH
 
 # Start conan server and mount _data sub-directory to conan storage path
-docker run  --detach \
-            --rm \
+
+ID=$(docker run  --detach \
+            --restart always \
+            --net $NETWORK \
             --publish 9300:9300 \
             --volume /$DATA_DIR:/var/lib/conan \
             --name $SERVER_NAME \
-            $IMAGE_NAME
+            --hostname $SERVER_NAME \
+            $IMAGE_NAME \
+    )
 
+echo $ID >&2
 echo "Server started!"
+
+sleep 5
