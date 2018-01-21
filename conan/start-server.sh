63e678601d80eab2227f9b8cc9f66642ad03ffc7
Fred <helmesjo@live.com>
2018-01-21 21:53:20 +0100

    Fixed so that the conan server config file (server.conf) is updated when starting the server to contain the correct host_name

--- conan/start-server.sh
+++ conan/start-server.sh
@@ -34,8 +34,10 @@ IMAGE_ID=$($REPO_ROOT/docker/build-image.sh $DOCKERFILE $SERVER_NAME 2>&1 >&3)
 # Copy server-config into to-be-mounted conan server-folder
 DATA_DIR="$DIR/_data"
 CONFIG_FILE="$DIR/server/server.conf"
-CONFIG_CONAN_PATH="$DATA_DIR/.conan_server/"
+CONFIG_CONAN_PATH="$DATA_DIR/.conan_server/server.conf"
 mkdir -p $CONFIG_CONAN_PATH && cp $CONFIG_FILE $CONFIG_CONAN_PATH
+# Set correct hostname used by the server
+sed -i "s/host_name:.*/host_name: $SERVER_NAME/g" $CONFIG_CONAN_PATH
 
 echo -e "\n-- Starting conan server & connecting it to network '$NETWORK'...\n"
 
