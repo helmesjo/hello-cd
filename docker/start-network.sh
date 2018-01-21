06643b76f713c11a9845ee1ebe72dbb3e8fd7464
Fred <helmesjo@live.com>
2018-01-21 02:39:28 +0100

    Changed ./docker/start-network.sh to start an overlay network (instead of bridge), using docker swarm. A container ID/name can be passed to the script, which will add that container as a manager to the swarm (used for gocd-agent & docker-in-docker so that the network is visible inside the docker-agent)

--- docker/start-network.sh
+++ docker/start-network.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 function on_error {
     echo "Could not start network $NETWORK_NAME" >&2
+    sleep 5
     exit 1
 }
 trap on_error ERR
@@ -16,10 +17,33 @@ NETWORK_ID=$(docker network ls --quiet --filter name=$NETWORK_NAME)
 
 # If network doesn't exist yet, start it
 if [ -z "${NETWORK_ID-}" ]; then
-    NETWORK_ID=$(docker network create $NETWORK_NAME)
+    docker node ls >/dev/null 2>&1 || FAILED=$? || true
+    if [ "${FAILED-}" ]; then
+        echo -e "\n-- Initiating swarm and joining as manager...\n"
+        docker swarm init
+    fi
+
+    NETWORK_ID=$(docker network create \
+                                    --driver overlay \
+                                    --attachable \
+                                    $NETWORK_NAME \
+                )
+    
     echo -e "\n-- Docker network '$NETWORK_NAME' started.\n"
 else
     echo -e "\n-- Docker network '$NETWORK_NAME' already started."
+fi
+
+# Container id/name passed is added as swarm managers
+CONTAINER="${1:-}"
+if [ "${CONTAINER-}" ]; then
+    echo -e "\n-- Adding container '$CONTAINER' as manager to swarm.\n"
+    SWARM_IP=$(docker node inspect self --format '{{ .Status.Addr  }}')
+    JOIN_TOKEN=$(docker swarm join-token manager --quiet)
+    
+    docker exec $CONTAINER docker swarm join --token $JOIN_TOKEN $SWARM_IP:2377 >/dev/null
+
+    echo -e "-- Container '$CONTAINER' added as manager to swarm.\n"
 fi
 
 echo $NETWORK_NAME >&2
