e4755bb1dc438e49193c5c89ceeb301118ab4b5a
helmesjo <helmesjo@live.com>
2018-01-31 22:26:30 +0100

    Fixed docker/stop-network.sh to properly remove network (if available) and prune unused networks

--- docker/stop-network.sh
+++ docker/stop-network.sh
@@ -16,12 +16,12 @@ echo -e "\n-- Stopping docker network '$NETWORK_NAME'..."
 
 NETWORK_ID=$(docker network ls --quiet --filter name=$NETWORK_NAME)
 
-# If network doesn't exist yet, start it
 if [ -z "${NETWORK_ID-}" ]; then    
     echo -e "\n-- Docker network '$NETWORK_NAME' not running.\n"
 else
     docker swarm leave --force 2>/dev/null || true
-    docker network remove $NETWORK_NAME
+    docker network remove $NETWORK_NAME >/dev/null 2>&1 || true
+    docker network prune -f >/dev/null
     echo -e "-- Docker network '$NETWORK_NAME' stopped."
 fi
 
