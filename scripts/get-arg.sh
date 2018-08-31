434f48c3374fd20d37a32595a6699b01dbe6c85a
helmesjo <helmesjo@live.com>
2018-08-31 23:26:37 +0200

    Changed and improved behavior of get-arg.sh. Less output & errors. Now returns value for '--flag=value' and 1 for '--flag', else nothing

--- scripts/get-arg.sh
+++ scripts/get-arg.sh
@@ -14,34 +14,21 @@ function on_error {
 trap on_error ERR
 
 ARGS=${1:-}
-
-if [ -z "${ARGS-}" ]; then
-    (>&2 echo "No arguments forwarded. In caller, do: 'get-arg.sh \"\$@\" --flag'")
-    on_error
-fi
-
 EXPECTED_ARG="${2:-}"
-
-if [ -z "${EXPECTED_ARG-}" ]; then
-    (>&2 echo "Must pass name of expected flag, eg: 'get-arg.sh --flag'")
-    on_error
-fi
 
 for i in ${ARGS[@]}
 do
    case "$i" in
         $EXPECTED_ARG=*)
             ARG="${i#*=}"
-            if [ -z "${EXPECTED_ARG-}" ]; then
-                on_error
-            else
-                (>&2 echo $ARG)
-                exit 0
-            fi
+            (>&2 echo $ARG)
+            exit 0
+            ;;
+        $EXPECTED_ARG)
+            (>&2 echo 1)
+            exit 0
             ;;
         *)
     esac
   shift
 done
-
-on_error
