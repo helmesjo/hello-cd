25860363f9d1ac59f820e3d2e76d317000690fe1
helmesjo <helmesjo@live.com>
2018-09-02 10:16:41 +0200

    Added fallback-value to get-arg.sh (third parameter)

--- scripts/get-arg.sh
+++ scripts/get-arg.sh
@@ -3,31 +3,34 @@
 set -euo pipefail
 
 function on_error {
-    ARG="<no-flag>"
-    if [ "${EXPECTED_ARG-}" ]; then
-        ARG="$EXPECTED_ARG"
-    fi
-
-    (>&2 echo "Could not find value for flag '$ARG'")
+    echo "Something failed while looking for flag '${EXPECTED_ARG-}' in '${ARGS-}'"
     exit 1
 }
 trap on_error ERR
 
-ARGS=${1:-}
+ARGS="${1:-}"
 EXPECTED_ARG="${2:-}"
+FALLBACK_VALUE="${3:-}"
+
+VALUE=${FALLBACK_VALUE:-}
 
 for i in ${ARGS[@]}
 do
-   case "$i" in
+    case "$i" in
+        # --flag=value
         $EXPECTED_ARG=*)
             ARG="${i#*=}"
-            (>&2 echo $ARG)
-            exit 0
+            VALUE=$ARG
+            break
             ;;
+        # --flag
         $EXPECTED_ARG)
-            (>&2 echo 1)
-            exit 0
+            VALUE=1
+            break
             ;;
         *)
     esac
 done
+
+# If a value was found, or if a fallback was specified, output it
+( [ -z "$VALUE" ] || >&2 echo $VALUE )
